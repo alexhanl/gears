f49c0f27bc59f568f440f072b0b54c6606f56bf8
Liang Han <liang.han@gmail.com>
2020-01-09 22:38:40 +0800

    delete the nfs support for simplification

--- quickinstall/nfs_managed_storageclass.sh
+++
@@ -1,9 +0,0 @@
-# wget https://get.helm.sh/helm-v3.0.2-darwin-amd64.tar.gz
-# tar zxvf helm-v3.0.2-darwin-amd64.tar.gz
-# sudo mv darwin-amd64/helm /usr/local/bin/helm
-
-# helm repo add stable https://kubernetes-charts.storage.googleapis.com
-
-scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null vagrant@kmaster:/home/vagrant/.kube/config ~/.kube/config
-
-helm install nfs-storage stable/nfs-client-provisioner -n kube-system --set nfs.server=kmaster --set nfs.path=/nfs-share  --set storageClass.name=managed-nfs-storage --set storageClass.defaultClass=true --set storageClass.archiveOnDelete=false
--- quickinstall/nfs_server_kmaster.sh
+++
@@ -1,6 +0,0 @@
-mkdir /nfs-share
-chmod -R 777 /nfs-share
-echo "/nfs-share 172.42.42.0/24(rw,sync,no_subtree_check,no_root_squash)" >> /etc/exports
-systemctl restart rpcbind.service 
-systemctl restart nfs.service
-showmount -e
