104cd631c8d0431e9dd1eaa9ac823bf0d1b924c8
Liang Han <liang.han@gmail.com>
2020-01-09 14:56:58 +0800

    typo fixed

--- quickinstall/nfs_managed_storageclass.sh
+++ quickinstall/nfs_managed_storageclass.sh
@@ -4,4 +4,6 @@
 
 # helm repo add stable https://kubernetes-charts.storage.googleapis.com
 
-helm install nfs-storage stable/nfs-client-provisioner -n kube-system --set nfs.server=kmaster --set nfs.path=/nfs-share  --set storageClass.name=managed-nfs-storage --set storageClass.defaultClass=true --storageClass.archiveOnDelete=false
+scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null vagrant@kmaster:/home/vagrant/.kube/config ~/.kube/config
+
+helm install nfs-storage stable/nfs-client-provisioner -n kube-system --set nfs.server=kmaster --set nfs.path=/nfs-share  --set storageClass.name=managed-nfs-storage --set storageClass.defaultClass=true --set storageClass.archiveOnDelete=false
